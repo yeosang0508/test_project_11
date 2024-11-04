f5d4010bd272cb45d6470b76e7e6c026d6304bcd
yeosang0508 <yeosang0508@gmail.com>
2024-11-04 15:17:16 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/surl_project_11/SurlProject11Application.java
@@ -0,0 +1,13 @@
+package com.example.surl_project_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurlProject11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurlProject11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/surl_project_11/SurlProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.surl_project_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurlProject11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
